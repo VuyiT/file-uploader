4cbcf0cab5cd31771f3e5290e4233b51cd178ec3
Avuyile Tiki <avuyiletiki14@gmail.com>
2026-04-03 12:30:10 +0200

    Setup prisma session store and express session

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
 require("dotenv").config();
 const express = require("express");
+const expressSession = require("express-session");
+const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
+const passport = require("passport");
+const { PrismaClient } = require("../generated/prisma/client.js")
 const path = require("node:path");
 const app = express();
 const PORT = process.env.PORT;
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+app.use(expressSession({
+    cookie: {
+        maxAge: 7 * 24 * 60 * 60 * 1000
+    },
+    secret: process.env.SECRET,
+    resave: true,
+    saveUninitialized: true,
+    store: new PrismaSessionStore(
+        new PrismaClient(),
+        {
+            checkPeriod: 2 * 60 * 1000,
+            dbRecordIdIsSessionId: true,
+            dbRecordIdFunction: undefined,
+        }
+    )
+}));
+app.use(passport.session());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => res.send("File Uploader"));
 
