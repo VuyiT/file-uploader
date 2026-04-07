4d982fe7d015a3b656293082ed1036b9e400ad9a
Avuyile Tiki <avuyiletiki14@gmail.com>
2026-04-07 18:51:08 +0200

    Reorder error json keys

--- middleware/errorMiddleware.js
+++ middleware/errorMiddleware.js
@@ -4,11 +4,11 @@ const globalErrorHandler = (err, req, res, next) => {
 
     if(process.env.NODE_ENV === "development") {
         res.status(err.statusCode).json({
+            name: err.name,
             status: err.status,
             error: err,
             message: err.message,
             stack: err.stack,
-            name: err.name
         });
     }
     else {
