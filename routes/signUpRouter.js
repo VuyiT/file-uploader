7f4154e68dc4440d71f4acac2ea03c124dff6140
Avuyile Tiki <avuyiletiki14@gmail.com>
2026-04-07 18:00:40 +0200

    Create route for postSignUp controller

--- routes/signUpRouter.js
+++ routes/signUpRouter.js
@@ -2,5 +2,6 @@ const signUpRouter = require("express").Router();
 const signUpController = require("../controllers/signUpController");
 
 signUpRouter.get("/", signUpController.getSignUp);
+signUpRouter.post("/", signUpController.postSignUp);
 
 module.exports = signUpRouter;
