1da502515a0c915aea781e13a918b436b1c122f3
Avuyile Tiki <avuyiletiki14@gmail.com>
2026-04-11 13:58:18 +0200

    Create postUpload controller

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -1,3 +1,5 @@
+const prisma = require("../lib/prisma");
+
 async function getUpload(req, res, next) {
     try {
         res.render("upload-file-form", {
@@ -8,6 +10,29 @@ async function getUpload(req, res, next) {
     }
 }
 
+async function postUpload(req, res, next) {
+    try {
+        const { originalname, size, mimetype, path } = req.file;
+        await prisma.file.create({
+            data: {
+                name: originalname,
+                size: size,
+                type: mimetype,
+                path: path,
+                user: {
+                    connect: {
+                        id: req.user.id
+                    }
+                }
+            }
+        });
+        res.redirect("/");
+    } catch (err) {
+        next(err);
+    }
+}
+
 module.exports = {
     getUpload,
+    postUpload,
 }
