0dcd6510532a1eede45572d29ce796c8aecd4d4f
Avuyile Tiki <avuyiletiki14@gmail.com>
2026-04-11 13:58:49 +0200

    Create route for postUpload controller

--- routes/uploadRouter.js
+++ routes/uploadRouter.js
@@ -1,6 +1,9 @@
 const uploadRouter = require("express").Router();
 const uploadController = require("../controllers/uploadController");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
 uploadRouter.get("/", uploadController.getUpload);
+uploadRouter.post("/", upload.single("uploadedFile"), uploadController.postUpload);
 
 module.exports = uploadRouter;
