eefe1365f60932060f10d560629e9fec52644658
Avuyile Tiki <avuyiletiki14@gmail.com>
2026-04-13 09:04:17 +0200

    Create logoutRouter

---
+++ routes/logoutRouter.js
@@ -0,0 +1,10 @@
+const logoutRouter = require("express").Router();
+
+logoutRouter.get("/", (req, res, next) => {
+    req.logout((err) => {
+        if (err) return next(err);
+        res.redirect("/");
+    });
+});
+
+module.exports = logoutRouter;
