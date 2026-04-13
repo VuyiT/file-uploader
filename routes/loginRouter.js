14bec2f90029be66bff45c5d3678126f7c969b1c
Avuyile Tiki <avuyiletiki14@gmail.com>
2026-04-13 09:03:48 +0200

    Change successRedirect to root path

--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -5,7 +5,7 @@ const loginController = require("../controllers/loginController");
 
 loginRouter.get("/", loginController.getLogin);
 loginRouter.post("/", passport.authenticate("local", {
-    successRedirect: "/upload-file",
+    successRedirect: "/",
     failureRedirect: "/log-in",
     failureFlash: true,
 }));
