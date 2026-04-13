9967b3259691731e19deba5deeb2860b507e3c6f
Avuyile Tiki <avuyiletiki14@gmail.com>
2026-04-13 14:47:26 +0200

    Create postFolder controller

--- controllers/folderController.js
+++ controllers/folderController.js
@@ -1,3 +1,5 @@
+const prisma = require("../lib/prisma");
+
 async function getCreateFolder(req, res, next) {
     try {
         res.render("create-folder-form", {
@@ -8,6 +10,25 @@ async function getCreateFolder(req, res, next) {
     }
 }
 
+async function postFolder(req, res, next) {
+    try {
+        await prisma.folder.create({
+            data: {
+                name: req.body.folderName,
+                user: {
+                    connect: {
+                        id: req.user.id,
+                    }
+                }
+            }
+        }); 
+        res.redirect("/");
+    } catch (err) {
+        next(err);
+    }
+}
+
 module.exports = {
     getCreateFolder,
+    postFolder,
 }
