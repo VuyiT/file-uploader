453aa73178452cf6dc5c05707f6a713aaab1d73e
Avuyile Tiki <avuyiletiki14@gmail.com>
2026-04-13 14:47:48 +0200

    Create route for postFolder controller

--- routes/folderRouter.js
+++ routes/folderRouter.js
@@ -2,5 +2,6 @@ const folderRouter = require("express").Router();
 const folderController = require("../controllers/folderController");
 
 folderRouter.get("/", folderController.getCreateFolder);
+folderRouter.post("/", folderController.postFolder);
 
 module.exports = folderRouter;
