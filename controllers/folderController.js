e10ebf9230e48915f08c1ff008fc118b90ceffc6
Avuyile Tiki <avuyiletiki14@gmail.com>
2026-04-14 10:13:36 +0200

    Create getViewFolder controller

--- controllers/folderController.js
+++ controllers/folderController.js
@@ -1,3 +1,4 @@
+const NotFoundError = require("../errors/NotFoundError");
 const prisma = require("../lib/prisma");
 
 async function getCreateFolder(req, res, next) {
@@ -28,7 +29,28 @@ async function postFolder(req, res, next) {
     }
 }
 
+async function getViewFolder(req, res, next) {
+    const folder = await prisma.folder.findUnique({
+        where: { id: Number(req.params.folderId) },
+        include: {
+            files: true,
+        }
+    });
+    if (!folder) {
+        return next(new NotFoundError("folder"));
+    }
+    try {
+        res.render("view-folder", {
+            title: folder.name,
+            folder,
+        });
+    } catch (err) {
+        next(err);
+    }
+}
+
 module.exports = {
     getCreateFolder,
     postFolder,
+    getViewFolder,
 }
