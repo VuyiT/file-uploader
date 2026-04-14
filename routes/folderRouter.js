4a4aafa443c6f357470212d2bcaa9f2a8ad1a16e
Avuyile Tiki <avuyiletiki14@gmail.com>
2026-04-14 18:22:34 +0200

    Use fileController.getFileUploadForm instead of folderController.getUploadFolderFileForm

--- routes/folderRouter.js
+++ routes/folderRouter.js
@@ -1,10 +1,11 @@
 const folderRouter = require("express").Router();
 const folderController = require("../controllers/folderController");
+const fileController = require("../controllers/fileController");
 
 
 folderRouter.get("/create", folderController.getCreateFolder);
 folderRouter.post("/create", folderController.postFolder);
 folderRouter.get("/:folderId", folderController.getViewFolder)
-folderRouter.get("/:folderId/upload-file", folderController.getUploadFolderFileForm);
+folderRouter.get("/:folderId/file/upload", fileController.getFileUploadForm);
 
 module.exports = folderRouter;
