79ac03fe82a2ba6e00cd372565330aa018730ff9
Avuyile Tiki <avuyiletiki14@gmail.com>
2026-04-14 10:23:53 +0200

    Create route for getUploadFolderFileForm controller

--- routes/folderRouter.js
+++ routes/folderRouter.js
@@ -5,5 +5,6 @@ const folderController = require("../controllers/folderController");
 folderRouter.get("/create", folderController.getCreateFolder);
 folderRouter.post("/create", folderController.postFolder);
 folderRouter.get("/:folderId", folderController.getViewFolder)
+folderRouter.get("/:folderId/upload-file", folderController.getUploadFolderFileForm);
 
 module.exports = folderRouter;
