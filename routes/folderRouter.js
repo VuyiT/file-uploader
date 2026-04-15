e630c4889ac8b80025e76ef7b5c87b9d1de3e36d
Avuyile Tiki <avuyiletiki14@gmail.com>
2026-04-15 09:05:05 +0200

    Register fileRouter and refine paths to /upload

--- routes/folderRouter.js
+++ routes/folderRouter.js
@@ -1,11 +1,14 @@
 const folderRouter = require("express").Router();
 const folderController = require("../controllers/folderController");
 const fileController = require("../controllers/fileController");
+const fileRouter = require("./fileRouter");
 
+folderRouter.use("/:folderId/file", fileRouter);
 
 folderRouter.get("/create", folderController.getCreateFolder);
 folderRouter.post("/create", folderController.postFolder);
 folderRouter.get("/:folderId", folderController.getViewFolder)
-folderRouter.get("/:folderId/file/upload", fileController.getFileUploadForm);
+folderRouter.get("/upload", fileController.getFileUploadForm);
+folderRouter.post("/upload", fileController.postFile);
 
 module.exports = folderRouter;
