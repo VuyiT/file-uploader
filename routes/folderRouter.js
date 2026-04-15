6e2e790467c514fabe930c2f5cff291a57c30601
Avuyile Tiki <avuyiletiki14@gmail.com>
2026-04-15 18:10:35 +0200

    Create route for getUpdateForm controller

--- routes/folderRouter.js
+++ routes/folderRouter.js
@@ -7,8 +7,10 @@ folderRouter.use("/:folderId/file", fileRouter);
 
 folderRouter.get("/create", folderController.getCreateFolder);
 folderRouter.post("/create", folderController.postFolder);
+folderRouter.get("/:folderId/update", folderController.getUpdateForm);
 folderRouter.get("/:folderId", folderController.getViewFolder)
 folderRouter.get("/upload", fileController.getFileUploadForm);
 folderRouter.post("/upload", fileController.postFile);
+
 
 module.exports = folderRouter;
